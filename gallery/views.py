c8b96666df7caf7b432f07267134a36fed4c32fd
Tucker Jones <urbanrunic@gmail.com>
2011-07-01 14:01:28 -0400

    Changing gallery views to add links to gallery-detail page

--- gallery/views.py
+++ gallery/views.py
@@ -7,12 +7,13 @@ from django.views.generic.list_detail import object_list
 from gallery.models import Gallery, Photo
 
 def gallery(request):
-    gallery = Gallery.objects.all()
-    context = RequestContext(request, {'gallery': gallery})    
+    galleries = Gallery.objects.all()
+    context = RequestContext(request, {'galleries': galleries})    
     return render_to_response('gallery/gallery.html', context)
 
 def gallery_detail(request, id):
+	galleries = Gallery.objects.all()
     gallery = get_object_or_404(Gallery, pk=id)
     images = Photo.objects.filter(gallery=gallery)
-    context = RequestContext(request, {'images': images})
+    context = RequestContext(request, {'images': images, 'galleries': galleries})
     return render_to_response('gallery/gallery-detail.html', context)
